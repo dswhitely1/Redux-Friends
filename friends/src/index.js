9c20cb8ce2f2135e967ca168529ec773206e2aef
Donald Whitely <dswhitely1@gmail.com>
2019-06-20 10:55:25 -0400

    Created App.jsx

--- friends/src/index.js
+++ friends/src/index.js
@@ -7,8 +7,8 @@ import store from './store';
 
 import App from './components/App';
 
-const application  = <Provider
-  store={store}><Router><App /></Router></Provider>;
+const application  = (<Provider
+  store={store}><Router><App /></Router></Provider>);
 const rootDocument = document.getElementById( 'root' );
-
+console.log(application);
 render( application, rootDocument );
