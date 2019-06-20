d91f589e7470d3fc4f37b56b5c8341937c3b9f1e
Donald Whitely <dswhitely1@gmail.com>
2019-06-20 11:05:21 -0400

    Defined the initial state of the application

--- friends/src/reducers/friendsReducer.js
+++ friends/src/reducers/friendsReducer.js
@@ -1,4 +1,12 @@
-const initialState = {};
+const initialState = {
+  deletingFriend: false,
+  fetchingFriends: false,
+  friends: [],
+  loggingIn: false,
+  savingFriends: false,
+  updatingFriend: false,
+  error: null
+};
 
 export default ( state = initialState, action ) => {
   const {type, payload} = action;
