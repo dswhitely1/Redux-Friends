61c1229c77fe7b3733fda89689d01e09cef31d8d
Donald Whitely <dswhitely1@gmail.com>
2019-06-20 11:00:18 -0400

    Fixed Random Error Messages

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import { friendsReducer as friends } from './friendsReducer';
+import friendsReducer  from './friendsReducer';
 
 export default combineReducers( {
-  friends
+  friends : friendsReducer
 } );
