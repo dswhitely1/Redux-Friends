29ed366d1cc395b094303ee1a82cacdb1806d42a
Donald Whitely <dswhitely1@gmail.com>
2019-06-20 10:49:20 -0400

    Created React's entry point (index.js)

---
+++ friends/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import store from './store';
+
+import App from './components/App';
+
+const application  = <Provider
+  store={store}><Router><App /></Router></Provider>;
+const rootDocument = document.getElementById( 'root' );
+
+render( application, rootDocument );
