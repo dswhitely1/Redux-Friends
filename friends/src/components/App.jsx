9f20a567d14fa345e9713bc2999bfbb733d99cb6
Donald Whitely <dswhitely1@gmail.com>
2019-06-20 16:57:46 -0400

    PrivateRoute component made

--- friends/src/components/App.jsx
+++ friends/src/components/App.jsx
@@ -4,11 +4,14 @@ import {Route} from 'react-router-dom';
 
 import FriendLogin from './friends/FriendLogin';
 import Container from 'react-bootstrap/Container';
+import PrivateRoute from './util/PrivateRoute';
 
 const App = () => {
+  var FriendContainer;
   return (
     <Container>
       <Route path='/login' component={FriendLogin} />
+      <PrivateRoute path='/dashboard' component={FriendContainer} />
     </Container>
   );
 };
